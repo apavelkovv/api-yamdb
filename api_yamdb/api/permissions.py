55c91a432edec1141bc03b336c5695a5344f81de
1234 <a1234@MacBook-Pro-1234.local>
2026-07-17 17:04:01 +0300

    Добавлены эндпоинты и вьюсеты

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Разрешает изменение только администратору, чтение — всем."""
+    
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+
+class IsAdmin(permissions.BasePermission):
+    """Разрешает доступ только администратору."""
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
